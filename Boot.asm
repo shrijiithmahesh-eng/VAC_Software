bbf0bdd2d5019335faca9747eb368b18a92f2969
Slabs_createor <shrijiith.mahesh@gmail.com>
2026-03-06 16:15:31 +0530

    Update Boot.asm

--- Boot.asm
+++ Boot.asm
@@ -2,7 +2,7 @@
 [org 0x7c00]  
 ; --- The Start of Project VAC ---
 start:
-mov ah, 0x0E  ; Tell BIOS we want to print a character
+  mov ah, 0x0E  ; Tell BIOS we want to print a character
 int 0x10        ; Call the Video Interrupt
 
 mov al, 'V'     ; Load 'V'
@@ -13,5 +13,18 @@ int 0x10        ; Print it
 
 mov al, 'c'     ; Load 'c'
 int 0x10         ;print it
-times 510-($-$$) db 0   ; Fill with 0s until byte 510
+; ... (After your 'Vac' and Loading Bar code)
+
+load_kernel:
+    mov ah, 0x02    ; BIOS 'Read Disk' function
+    mov al, 1       ; Read 1 sector (The Kernel)
+    mov ch, 0       ; Cylinder 0
+    mov dh, 0       ; Head 0
+    mov cl, 2       ; Sector 2 (Right after the Bootloader!)
+    mov bx, 0x1000  ; Memory address to drop the Kernel
+    mov es, bx
+    xor bx, bx
+    int 0x13        ; DISK INTERRUPT!
+
+    jmp 0x1000:0000 ; THE JUMP TO THE KERNEL!
 dw 0xAA55               ; The Magic Boot Signature (Bytes 511 and 512)
