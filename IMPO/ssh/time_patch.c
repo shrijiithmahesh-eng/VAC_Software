34dfc14d0d80e92ea882b35917ecd762844066e0
Slabs_createor <shrijiith.mahesh@gmail.com>
2026-03-14 16:43:46 +0530

    Update time_patch.c

--- IMPO/ssh/time_patch.c
+++ IMPO/ssh/time_patch.c
@@ -1,8 +1,21 @@
-/* VAC OS: OPENSSH LATENCY PATCH */
-#define MARS_LATENCY_BUFFER 1200 // 20 minutes in seconds
+/* VAC OS: TIME_PATCH.C */
 
-void adjust_ssh_timeouts() {
-    // Override the "Modern" 2-minute timeout
-    current_ssh_session->timeout = MARS_LATENCY_BUFFER;
-    log_event("SSH_PROTOCOL");
+#include <sys/socket.h>
+#include <sys/time.h>
+
+#define MARS_LATENCY_MAX 1800 // 30 Minutes of Patience
+
+void apply_latency_buffer(int socket_fd) {
+    struct timeval timeout;      
+    timeout.tv_sec = MARS_LATENCY_MAX;
+    timeout.tv_usec = 0;
+
+    // 1. Tell the OS to wait 30 minutes for a response
+    setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+    
+    // 2. Tell the OS to wait 30 minutes to send data
+    setsockopt(socket_fd, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));
+
+    // 3. Log the successful temporal shift
+    log_event("TEMPORAL_PATCH_APPLIED: SPACE_MODE_ACTIVE");
 }
