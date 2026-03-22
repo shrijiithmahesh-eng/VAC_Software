b696f9a152a788bbef364fa2526574d9a6e6632c
Slabs_createor <249469506+shrijiithmahesh-eng@users.noreply.github.com>
2026-03-22 15:45:46 +0530

    Update Boot.asm

--- Boot.asm
+++ Boot.asm
@@ -1,6 +1,6 @@
 [bits 16]
 [org 0x7c00]  
-; --- The Start of Project VAC ---
+; --- The Start of Project VAC---
 start:
   mov ah, 0x0E  ; Tell BIOS we want to print a character
 int 0x10        ; Call the Video Interrupt
