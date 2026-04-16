d1ff35cf83c970ea69378be7a83994aa8360e33b
Slabs_createor <249469506+shrijiithmahesh-eng@users.noreply.github.com>
2026-04-16 16:41:54 +0530

    Update Masssuckerverify.asm

--- IMPO/Sucktion/Masssuckerverify.asm
+++ IMPO/Sucktion/Masssuckerverify.asm
@@ -1,84 +1,42 @@
-; VAC OS: FINAL TORNADO EVASION INTEGRATION
-; Input: EAX = Distance (cm)
-; --- ADD THIS BACK AT THE TOP ---
-check_vortex_proximity:
-    test eax, eax           ; Check if distance is 0
-    jz   emergency_stop     ; If distance is 0, the tornado is ON TOP of us!
-    
-    cmp  eax, PROXIMITY_THRESHOLD
-    jle  start_evasion      ; If <= 92cm, start the math
-    ret                     ; Otherwise, we are safe (for now)
+; VAC OS: 32-BIT SHUTDOWN PROCEDURE
+; Architect: Shrijiith Mahesh
 
-start_evasion:
-    call calculate_thrust_duration
-    call execute_thrust
-    ret
+[BITS 32]
 
-emergency_stop:
-    cli                     ; Stop all interrupts
-    mov al, 0xFF            ; Diagnostic port signal
-    out 0x80, al
+section .text
+global shutdown_sequence
 
-    ; --- BIOS Print "this the end" ---
-    mov ah, 0x0E            ; BIOS Teletype function
-    
-    mov al, 't'
-    int 0x10                ; Print 't'
-    mov al, 'h'
-    int 0x10                ; Print 'h'
-    mov al, 'i'
-    int 0x10
-    mov al, 's'
-    int 0x10
-    mov al, ' '             ; Space character
-    int 0x10
-    mov al, 't'
-    int 0x10
-    mov al, 'h'
-    int 0x10
-    mov al, 'e'
-    int 0x10
-    mov al, ' '
-    int 0x10
-    mov al, 'e'
-    int 0x10
-    mov al, 'n'
-    int 0x10
-    mov al, 'd'
-    int 0x10
+VIDEO_MEMORY equ 0xB8000
+WHITE_ON_BLACK equ 0x0F
 
-    hlt                     ; Final system halt. Mission Over.
-PROXIMITY_THRESHOLD equ 92
-; 0.333 seconds = 333 milliseconds. 
-; So, Burn Time (ms) = (92 - distance) * 333
-MS_MULTIPLIER       equ 333 
-THRUSTER_PORT       equ 0x3F
+shutdown_sequence:
+    ; 1. Kill Thrusters immediately
+    mov dx, 0x3F
+    mov al, 0x00
+    out dx, al
 
-calculate_thrust_duration:
-    mov  ebx, PROXIMITY_THRESHOLD
-    sub  ebx, eax           ; EBX = distance delta in cm
-    imul ebx, MS_MULTIPLIER ; EBX = total burn time in MILLISECONDS
-    ret
+    ; 2. Print the Final Message to VGA Buffer
+    mov esi, msg_final
+    mov edi, VIDEO_MEMORY    ; Start at top-left of screen
+    call print_string_32
 
-execute_thrust:
+    ; 3. Final Halt
     cli
-    mov  dx, THRUSTER_PORT
-    mov  al, 0x01
-    out  dx, al             ; THRUSTERS ON
+    hlt
 
-    ; Now we need a delay loop that accurately waits 1ms per EBX unit
-.wait_ms:
-    push ebx
-    mov  ecx, 2500000       ; CALIBRATION: Adjust this for your Yoga 520 (2.5GHz-ish)
-.inner_loop:
-    dec  ecx
-    jnz  .inner_loop        ; This loop should take exactly 1ms
-    pop  ebx
-    
-    dec  ebx
-    jnz  .wait_ms           ; Repeat for every millisecond in EBX
+print_string_32:
+    .loop:
+        lodsb               ; Load byte from ESI into AL
+        test al, al         ; Check for null terminator
+        jz .done
+        
+        mov [edi], al       ; Write character
+        mov [edi+1], byte WHITE_ON_BLACK ; Write color attribute
+        add edi, 2          ; Move to next character cell (2 bytes per char)
+        jmp .loop
+    .done:
+        ret
 
-    mov  al, 0x00
-    out  dx, al             ; THRUSTERS OFF
-    sti
-    ret
+section .data
+    msg_final db 'STATUS: DARKNESS. GOODBYE, CHENNAI. [MATH FAIL: 0.333s/cm]', 0
+   
